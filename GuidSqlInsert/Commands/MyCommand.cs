409514709c4288a62b6a8049f961c7709e1e7398
yui <yui.han@devfacto.com>
2023-12-07 02:14:37 -0500

    Add initial code

---
+++ GuidSqlInsert/Commands/MyCommand.cs
@@ -0,0 +1,39 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using static System.Linq.Enumerable;
+using System;
+using Task = System.Threading.Tasks.Task;
+
+namespace GuidSqlInsert
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await Package.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var doc = await VS.Documents.GetActiveDocumentViewAsync();
+            var position = doc?.TextView?.Caret.Position.BufferPosition;
+            if (position.HasValue)
+            {
+                var input = doc.TextView.GetTextViewLineContainingBufferPosition(position.Value).Extent.GetText();
+
+                int guidcount;
+                var isnumber = int.TryParse(input, out guidcount);
+
+                var guidlist = GetGuidList(count: isnumber ? guidcount : 1);
+
+                if (isnumber && guidcount > 0)
+                {
+                    doc.TextBuffer?.Replace(doc.TextView.GetTextViewLineContainingBufferPosition(position.Value).Extent.Span, guidlist);
+                }
+                else
+                {
+                    doc.TextBuffer.Insert(position.Value, guidlist);
+                }
+            }
+        }
+
+        private string GetGuidList(int count = 1) => string.Join(",\n", Range(1, count).Select(c => $"('{Guid.NewGuid()}')"));
+    }
+}
---
+++ GuidSqlInsert/GuidSqlInsertPackage.cs
@@ -0,0 +1,21 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace GuidSqlInsert
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.GuidSqlInsertString)]
+    public sealed class GuidSqlInsertPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
+}
